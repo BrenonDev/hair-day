99c9588d38d9c287adaeda8836c31e45a16efab9
BrenonDev <brenon.olivetti.rondello@gmail.com>
2026-07-08 14:40:57 -0300

    renderizando os agendamentos do dia

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,5 +1,6 @@
 import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
 import { hoursLoad } from "../form/hours-load.js";
+import { schedulesShow } from "./show.js";
 
 // Seleciona o input de data
 const selectedDate = document.getElementById("date");
@@ -10,8 +11,9 @@ export async function schedulesDay() {
 
     // Busca na API os agendamentos
     const dailySchedules = await scheduleFetchByDay({ date });
-    console.log(dailySchedules);
     
+    // Exibe os agendamentos
+    schedulesShow({ dailySchedules });
 
     // Renderiza as horas disponíveis
     hoursLoad({date});
